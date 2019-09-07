8d58b7b1d8be2045f9b6adae16fc6293dbe12036
phianh8 <nguyentatphianh@gmail.com>
2019-09-07 20:00:11 +0700

    up conrua

---
+++ ss2/conrua.py
@@ -0,0 +1,13 @@
+from turtle import *
+
+color("red")
+shape("turtle")
+speed(-1)
+# for i in range(360):
+#     for i in range(4):
+#         forward(100)
+#         right(90)
+#     right(1)
+circle(100)
+
+mainloop()
